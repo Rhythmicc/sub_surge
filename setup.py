eb823e9cde5f6edeaa0323d574c7dd5811c292af
Rhythmicc <RhythmLian@outlook.com>
2022-09-28 01:56:11 +0800

    update

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=["Qpro"],
     entry_points={
         "console_scripts": [
-            "sub_surge = sub_surge.main:main",
+            "sub-surge = sub_surge.main:main",
         ]
     },
 )
