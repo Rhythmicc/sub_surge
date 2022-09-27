de68f3a53c5d81bc246edbf3ef2baff31c9bf436
Rhythmicc <RhythmLian@outlook.com>
2022-09-28 01:53:26 +0800

    first commit

---
+++ sub_surge/main.py
@@ -0,0 +1,157 @@
+from QuickProject.Commander import Commander
+from . import *
+
+app = Commander()
+
+
+@app.custom_complete("name")
+def update():
+    return [
+        {"name": i, "icon": "✈️", "description": config.select(i)["show_name"]}
+        for i in config.get_all()
+    ]
+
+
+@app.command()
+def update(name: str):
+    """
+    更新Surge配置文件
+
+    :param name: 机场名称
+    """
+    if not os.path.exists(".{name}.conf"):
+        with QproDefaultConsole.status("正在下载配置文件"):
+            requirePackage(
+                "QuickStart_Rhy.NetTools.NormalDL",
+                "normal_dl",
+                real_name="QuickStart_Rhy",
+            )(config.select(name)["url"], f".{name}.conf")
+
+    with open(f".{name}.conf", "r") as f:
+        content = [i.strip() for i in f.readlines()]
+    proxy_list = requirePackage(f".airports.{name}", "get_proxies_list")(content)
+    other_infos = requirePackage(f".airports.{name}", "get_other_infos")(content)
+
+    all_proxy_list = proxy_list.copy()
+    for item in other_infos:
+        for _id, line in enumerate(all_proxy_list):
+            if other_infos[item].strip() in line:
+                proxy_list.remove(line)
+                all_proxy_list[_id] = (
+                    f'{item}: {other_infos[item].split("=")[0].strip()} = '
+                    + "=".join(line.split("=")[1:]).strip()
+                )
+                break
+    with open(f".{name}.conf", "w") as f:
+        infos = {
+            "cos_url": f"{config.select('txcos_domain')}/{config.select(name)['key']}",
+            "proxies": "\n".join(all_proxy_list),
+            "infos": ",".join(
+                [f'{i}: {other_infos[i].split("=")[0].strip()}' for i in other_infos]
+            ),
+            "proxies_one_line": ",".join(
+                [i.split("=")[0].strip() for i in proxy_list if i]
+            ),
+            "proxies_one_line_hk": ",".join(
+                [i.split("=")[0].strip() for i in proxy_list if "香港" in i]
+            ),
+            "proxies_one_line_jp": ",".join(
+                [i.split("=")[0].strip() for i in proxy_list if "日本" in i]
+            ),
+            "proxies_one_line_us": ",".join(
+                [i.split("=")[0].strip() for i in proxy_list if "美国" in i]
+            ),
+            "proxies_one_line_sg": ",".join(
+                [i.split("=")[0].strip() for i in proxy_list if "新加坡" in i]
+            ),
+        }
+        from .template import conf_template
+
+        f.write(conf_template.format(**infos))
+    with QproDefaultConsole.status("正在上传配置文件"):
+        requirePackage(
+            "QuickStart_Rhy.API.TencentCloud",
+            "TxCOS",
+            real_name="QuickStart_Rhy",
+        )().upload(f".{name}.conf", config.select(name)["key"])
+    requirePackage("QuickStart_Rhy", "remove")(f".{name}.conf")
+    QproDefaultConsole.print(
+        QproInfoString,
+        f"更新成功, 链接: {config.select('txcos_domain')}/{config.select(name)['key']}",
+    )
+
+    if copy := requirePackage("pyperclip", "copy", not_ask=True):
+        copy(f"{config.select('txcos_domain')}/{config.select(name)['key']}")
+        QproDefaultConsole.print(QproInfoString, f"链接已复制到剪贴板")
+
+
+@app.command()
+def register(name: str):
+    """
+    添加机场
+
+    :param name: 机场名
+    """
+    from . import _ask
+
+    cur_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "airports")
+    if not os.path.exists(cur_path):
+        os.mkdir(cur_path)
+    airports = os.listdir(cur_path)
+
+    if (name in airports or config.select(name)) and _ask(
+        {"type": "confirm", "message": "此机场已注册, 是否覆盖?", "default": False}
+    ):
+        remove = requirePackage("QuickStart_Rhy", "remove")
+        remove(os.path.join(airports, f"{name}.py"))
+        config.update(name, None)
+
+    values = {
+        "url": _ask({"type": "input", "message": "输入机场订阅链接"}),
+        "key": _ask({"type": "input", "message": "输入腾讯云对应存储位置"}),
+        "show_name": _ask({"type": "input", "message": "输入机场描述信息", "default": name}),
+        "custom_format": _ask({"type": "input", "message": "输入自定义格式化文件路径"}),
+    }
+    if not os.path.exists(values["custom_format"]):
+        from QuickProject import QproErrorString
+
+        return QproDefaultConsole.print(QproErrorString, "自定义格式化文件不存在, 请重新输入")
+
+    values["custom_format"] = os.path.abspath(values["custom_format"])
+    import shutil
+
+    shutil.copy(values["custom_format"], os.path.join(cur_path, f"{name}.py"))
+    values.pop("custom_format")
+
+    config.update(name, values)
+    QproDefaultConsole.print(QproInfoString, "注册成功")
+
+
+@app.command()
+def unregister(name: str):
+    """
+    删除机场
+
+    :param name: 机场名
+    """
+    from . import _ask
+
+    cur_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "airports")
+    if config.select(name) and _ask(
+        {"type": "confirm", "message": "是否删除此机场?", "default": False}
+    ):
+        requirePackage("QuickStart_Rhy", "remove")(os.path.join(cur_path, f"{name}.py"))
+        config.update(name, None)
+        QproDefaultConsole.print(QproInfoString, "删除成功")
+
+
+def main():
+    """
+    注册为全局命令时, 默认采用main函数作为命令入口, 请勿将此函数用作它途.
+    When registering as a global command, default to main function as the command entry, do not use it as another way.
+    """
+    app()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sub_surge/template.py
@@ -0,0 +1,59 @@
+conf_template = """#!MANAGED-CONFIG {cos_url} interval=86400 strict=false
+[General]
+loglevel = notify
+bypass-system = true
+skip-proxy = 127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,100.64.0.0/10,localhost,*.local,e.crashlytics.com,captive.apple.com,::ffff:0:0:0:0/1,::ffff:128:0:0:0/1
+http-listen = 127.0.0.1:7891
+socks5-listen = 127.0.0.1:7890
+test-timeout = 5
+bypass-tun = 192.168.0.0/16,10.0.0.0/8,172.16.0.0/12
+dns-server = 119.29.29.29,223.5.5.5
+
+[Script]
+http-request https?:\/\/.*\.iqiyi\.com\/.*authcookie= script-path=https://raw.githubusercontent.com/NobyDa/Script/master/iQIYI-DailyBonus/iQIYI.js
+
+[Proxy]
+DIRECT = direct
+{proxies}
+
+[Proxy Group]
+📒 机场信息 = select,{infos}
+🚀 节点选择 = select,DIRECT,🔧 手动切换,🇭🇰 香港节点,🇯🇵 日本最佳,🇺🇸 美国最佳,🇸🇬 狮城最佳
+🔧 手动切换 = select,DIRECT,{proxies_one_line}
+🌍 国外媒体 = select,🚀 节点选择,🎯 全球直连,🇸🇬 狮城最佳,🇺🇸 美国最佳
+📲 电报信息 = select,🚀 节点选择,🎯 全球直连,{proxies_one_line}
+Ⓜ️ 微软服务 = select,🎯 全球直连,🚀 节点选择,{proxies_one_line}
+🍎 苹果服务 = select,🚀 节点选择,🎯 全球直连,{proxies_one_line}
+📢 谷歌FCM = select,🚀 节点选择,🎯 全球直连,{proxies_one_line}
+🎯 全球直连 = select,DIRECT,🚀 节点选择
+🛑 全球拦截 = select,REJECT,DIRECT
+🍃 应用净化 = select,REJECT,DIRECT
+🐟 漏网之鱼 = select,🚀 节点选择,🎯 全球直连,{proxies_one_line}
+🇭🇰 香港节点 = url-test,{proxies_one_line_hk},url=http://www.gstatic.com/generate_204,interval=300,tolerance=50
+🇯🇵 日本最佳 = url-test,{proxies_one_line_jp},url=http://www.gstatic.com/generate_204,interval=300,tolerance=50
+🇺🇸 美国最佳 = url-test,{proxies_one_line_us},url=http://www.github.com,interval=300,tolerance=50
+🇸🇬 狮城最佳 = url-test,{proxies_one_line_sg},url=http://www.gstatic.com/generate_204,interval=300,tolerance=50
+
+[Rule]
+DOMAIN-SUFFIX,cupbug.cloud,DIRECT
+DOMAIN-SUFFIX,microsoftonline.cn,DIRECT
+DOMAIN-SUFFIX,youtube.com,🚀 节点选择
+DOMAIN-SUFFIX,jp,🇯🇵 日本最佳
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/LocalAreaNetwork.list,🎯 全球直连,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/UnBan.list,🎯 全球直连,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list,🛑 全球拦截,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanProgramAD.list,🍃 应用净化,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/GoogleFCM.list,📢 谷歌FCM,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/GoogleCN.list,🎯 全球直连,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/SteamCN.list,🎯 全球直连,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Microsoft.list,Ⓜ️ 微软服务,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Apple.list,🍎 苹果服务,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Telegram.list,📲 电报信息,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ProxyMedia.list,🌍 国外媒体,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ProxyLite.list,🚀 节点选择,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ChinaDomain.list,🎯 全球直连,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ChinaCompanyIp.list,🎯 全球直连,update-interval=86400
+RULE-SET,https://raw.githubusercontent.com/Rhythmicc/ACL4SSR/master/Clash/us.list,🇺🇸 美国最佳,update-interval=86400
+GEOIP,CN,🎯 全球直连
+FINAL,🐟 漏网之鱼
+"""
